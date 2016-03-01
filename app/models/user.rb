d4ac2ff465ced972a6d82865acb0c01cf0ffd48f
Cairo Stewart <stewartcairo@gmail.com>
2016-02-29 16:26:38 -0800

    done with bgFix 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email, uniqueness: {case_sensitive: false}
-  validates :name, presence: true, uniqueness: {case_sensitive: false}
+  validates :name, :email, :password, presence: true, uniqueness: {case_sensitive: false}
 
   before_validation do
     self.email = email? ? email.downcase : nil
